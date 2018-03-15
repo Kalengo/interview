e87826444df1b60df3a2a3bbace1a3da6fbb0e12
sikichan <chenqiqi12138@gmail.com>
2018-03-15 23:33:41 +0800

    随机分组

---
+++ src/devide.js
@@ -0,0 +1,29 @@
+
+module.exports = function (arr, k) {
+
+  const groupNum = arr.length / k // 分k组
+
+  // 随机打乱heros数组的顺序
+  arr.sort(() => { 
+    return 0.5 - Math.random()
+  })
+
+  const map = new Map()
+
+  // 开始分组
+  for (let i = 0; i < groupNum; i++){
+    
+    const group = []
+    
+    for (let j = i; j < arr.length; j += k) {
+      if (arr[j]) {
+        group.push(arr[j])
+      }
+    }
+
+    map.set(`第${i+1}组`, group)
+  }
+  
+  return map
+
+}
